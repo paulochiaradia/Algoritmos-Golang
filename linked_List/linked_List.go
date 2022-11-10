2b17b7282b49265820587e41054f4683209b4de0
paulochiaradia <97291302+paulochiaradia@users.noreply.github.com>
2022-11-10 22:25:37 +0000

    Linked_List

--- linked_List/linked_List.go
+++ linked_List/linked_List.go
@@ -2,37 +2,68 @@ package main
 
 import "fmt"
 
+// List -Struct do tipo Lista
 type List struct {
 	Head *Node
 	Tail *Node
 }
 
+// Node - Struct do Tipo Nó
 type Node struct {
-	value Pessoa
+	Value Pessoa
 	Next  *Node
 }
 
-func (l *List) emptyHead() bool {
-	return l.Head == nil
-}
-func (l *List) emptyTail() bool {
-	return l.Tail == nil
-}
 func (l *List) Append(pessoa Pessoa) {
-	node := Node{value: pessoa}
-	if l.emptyHead() {
-		l.Head = &node
+	node := &Node{Value: pessoa}
+	if l.Head == nil {
+		l.Head = node
 	}
-	if l.emptyTail() {
-		l.Head.Next = &node
+	if l.Tail != nil {
+		l.Tail.Next = node
 	}
-	l.Tail = &node
+	l.Tail = node
 }
 
 func (l *List) Display() {
 	node := l.Head
+
 	for node != nil {
-		fmt.Println(node.value.Nome)
+		fmt.Printf("%s - %d\n", node.Value.Nome, node.Value.Idade)
 		node = node.Next
 	}
+}
+func (l *List) Search(nome string) int {
+	node := l.Head
+	for node != nil {
+		if node.Value.Nome == nome {
+			return 1
+		}
+		node = node.Next
+	}
+	return 0
+}
+
+func (l *List) Delete(nome string) {
+	if l.Search(nome) != 0 {
+		if l.Head.Value.Nome == nome {
+			l.Head = l.Head.Next
+			return
+		}
+		prev := l.Head
+		node := l.Head.Next
+
+		for node != nil {
+			if node.Value.Nome == nome {
+				prev.Next = node.Next
+				//proximo do anterior aponta para o meu proximo
+			}
+			prev = node
+			node = node.Next
+		}
+		if l.Tail == node {
+			l.Tail = prev
+		}
+	}
+
 }
--- linked_List/main.go
+++ linked_List/main.go
@@ -1,19 +1,40 @@
 package main
 
+import "fmt"
+
 type Pessoa struct {
-	Nome      string
-	Sobrenome string
-	Idade     int
+	Nome  string
+	Idade int
 }
 
 func main() {
 	list := List{}
-
-	Paulo := Pessoa{Nome: "Paulo", Sobrenome: "Neto", Idade: 21}
-	Aline := Pessoa{Nome: "Aline", Sobrenome: "Souza", Idade: 21}
-
+	Paulo := Pessoa{
+		Nome:  "Paulo",
+		Idade: 21,
+	}
+	Aline := Pessoa{
+		Nome:  "Aline",
+		Idade: 21,
+	}
+	Joao := Pessoa{
+		Nome:  "Joao",
+		Idade: 30,
+	}
+	Maria := Pessoa{
+		Nome:  "Maria",
+		Idade: 32,
+	}
 	list.Append(Paulo)
 	list.Append(Aline)
-
+	list.Append(Joao)
+	list.Append(Maria)
 	list.Display()
+	fmt.Println("--------------")
+	list.Delete("Eduardo")
+	list.Display()
+	fmt.Println("--------------")
+	list.Delete("Aline")
+	list.Display()
+
 }
